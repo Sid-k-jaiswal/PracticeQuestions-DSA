79ad5afc64129240ff48d87c6f768630178795ec
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2025-06-01 23:20:07 +0530

    Time: 0 ms (100%), Space: 17.8 MB (80.67%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = []
+        parenthesis_map = {']':'[','}':'{',')':'('}
+
+        for i in s:
+
+            if i in parenthesis_map:
+                top = stack.pop() if stack else '_'
+                if top != parenthesis_map[i]:
+                    return False
+            else:
+                stack.append(i)
+        
+        return stack == []
+        
