df630477c28c783040a0d59b4206ff91aa5f915c
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2025-06-02 23:35:58 +0530

    Time: 8 ms (52.91%), Space: 19.4 MB (23.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,4 +1,31 @@
 class Solution:
+
+    # def isAlphaNumeric(self, c):
+    #     return     (ord('A') <= ord(c) <= ord('Z') or
+    #                 ord('a') <= ord(c) <= ord('z') or
+    #                 ord('0') <= ord(c) <= ord('9'))
+
+    # def isPalindrome(self, s: str) -> bool:
+
+    #     # s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+
+    #     i, j = 0, len(s) - 1
+
+    #     while i < j:
+
+    #         while i < j and not self.isAlphaNumeric(s[i]):
+    #             i += 1
+    #         while i < j and not self.isAlphaNumeric(s[j]):
+    #             j -= 1
+
+    #         if s[i].lower() == s[j].lower():
+    #             i += 1
+    #             j -= 1
+    #         else:
+    #             return False
+        
+    #     return True
+
     def isPalindrome(self, s: str) -> bool:
 
         s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
@@ -6,7 +33,6 @@ class Solution:
         i, j = 0, len(s) - 1
 
         while i < j:
-
             if s[i] == s[j]:
                 i += 1
                 j -= 1
@@ -14,5 +40,4 @@ class Solution:
                 return False
         
         return True
-
         
