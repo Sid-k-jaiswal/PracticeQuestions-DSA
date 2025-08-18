3d4f9b5fd0fe901987a091607437b76ec1edf38c
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2025-08-18 23:20:33 +0530

    Time: 5 ms (53.25%), Space: 25.7 MB (46.48%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,35 +1,40 @@
 class Solution:
 
-    # def swap(self, nums, l, r):
-    #     while l < r:
-    #         nums[l], nums[r] = nums[r], nums[l]
-    #         l += 1
-    #         r -= 1
-    #     return nums
-
-    # def rotate(self, nums: List[int], k: int) -> None:
-    #     """
-    #     Do not return anything, modify nums in-place instead.
-    #     """
-
-    #     k = k % len(nums)
-
-    #     nums = self.swap(nums, 0, len(nums)-1)
-    #     nums = self.swap(nums, 0, k-1)
-    #     nums = self.swap(nums, k, len(nums)-1)
+    # [1,2,3,4,5,6,7], k = 3 ----> 
+    # l = 0, r = len -1 ---> [7,6,5,4,3,2,1]
+    # l=0, r = k-1 ---> [5,6,7,4,3,2,1]
+    # l=k, r = len - 1 ---> [5,6,7,1,2,3,4]
+    
+    def swap(self, nums, l, r):
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+        return nums
 
     def rotate(self, nums: List[int], k: int) -> None:
-        #     """
-        #     Do not return anything, modify nums in-place instead.
-        #     """
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
-        tmp_array = [0]*len(nums)
+        k = k % len(nums)
 
-        # k = k % len(nums)
+        nums = self.swap(nums, 0, len(nums)-1)
+        nums = self.swap(nums, 0, k-1)
+        nums = self.swap(nums, k, len(nums)-1)
 
-        for i in range(len(nums)):
-            tmp_array[(i+k)%len(nums)] = nums[i]
+    # def rotate(self, nums: List[int], k: int) -> None:
+    #     #     """
+    #     #     Do not return anything, modify nums in-place instead.
+    #     #     """
+
+    #     tmp_array = [0]*len(nums)
+
+    #     # k = k % len(nums)
+
+    #     for i in range(len(nums)):
+    #         tmp_array[(i+k)%len(nums)] = nums[i]
         
-        for i in range(len(nums)):
-            nums[i] = tmp_array[i]
+    #     for i in range(len(nums)):
+    #         nums[i] = tmp_array[i]
         
