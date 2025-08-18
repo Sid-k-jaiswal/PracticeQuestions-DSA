59c599d120e4c62ab9b5d6bd8ee487a6fc39dd3f
Siddhant <65653327+Sid-k-jaiswal@users.noreply.github.com>
2025-08-18 22:46:05 +0530

    Time: 5 ms (53.25%), Space: 25.8 MB (20.45%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,21 +1,35 @@
 class Solution:
 
-    def swap(self, nums, l, r):
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
-        return nums
+    # def swap(self, nums, l, r):
+    #     while l < r:
+    #         nums[l], nums[r] = nums[r], nums[l]
+    #         l += 1
+    #         r -= 1
+    #     return nums
+
+    # def rotate(self, nums: List[int], k: int) -> None:
+    #     """
+    #     Do not return anything, modify nums in-place instead.
+    #     """
+
+    #     k = k % len(nums)
+
+    #     nums = self.swap(nums, 0, len(nums)-1)
+    #     nums = self.swap(nums, 0, k-1)
+    #     nums = self.swap(nums, k, len(nums)-1)
 
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        #     """
+        #     Do not return anything, modify nums in-place instead.
+        #     """
+
+        tmp_array = [0]*len(nums)
 
         k = k % len(nums)
 
-        nums = self.swap(nums, 0, len(nums)-1)
-        nums = self.swap(nums, 0, k-1)
-        nums = self.swap(nums, k, len(nums)-1)
-
+        for i in range(len(nums)):
+            tmp_array[(i+k)%len(nums)] = nums[i]
+        
+        for i in range(len(nums)):
+            nums[i] = tmp_array[i]
         
